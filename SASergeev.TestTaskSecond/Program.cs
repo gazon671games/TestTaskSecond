cffc9fe55cd12e6befb92b9ab6e0b7261d8d44d8
ozon <ozon777@protonmail.com>
2021-07-27 19:34:59 +0300

    Add project files.

---
+++ SASergeev.TestTaskSecond/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SASergeev.TestTaskSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
